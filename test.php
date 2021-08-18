2b56384fe71ee4906699181b9aa0664de2d16281
Hoàng Phúc <=>
2021-08-18 10:35:59 +0700

    Phúc update ví dụ về json và phân chia công việc

--- test.php
+++ test.php
@@ -12,13 +12,18 @@
     */
     // Test POST
     // POST giống GET nhưng gửi dữ liệu dạng ẩn
-     
+    /*
     if ( !isset($_POST['value']) || !isset($_POST['value2']))
         return;
     
     $a = intval($_POST['value']);
     $b = intval($_POST['value2']);
     echo $a + $b;
-    
+    */
+    $input = file_get_contents('php://input');
+    if ($input=="")
+        return;
+    $json = json_decode($input, true);
+    echo $json["input1"];
 
 ?>
