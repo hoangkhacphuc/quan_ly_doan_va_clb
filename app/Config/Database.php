51b20897b38489e115a5ee4d60efa3681d4c3208
Hoàng Phúc <=>
2021-10-11 11:02:11 +0700

    edit database user pass

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'kenplaygirl',
+        'password' => 'Phuc123456',
+        'database' => 'ql_doan_clb',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
