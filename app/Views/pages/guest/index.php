1c8204f2987578057b1b5163ae9723fbc86e0efe
Hoàng Phúc <=>
2021-10-13 10:30:29 +0700

    fix js

--- app/Views/pages/guest/index.php
+++ app/Views/pages/guest/index.php
@@ -8,12 +8,12 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title><?= $title ?></title>
     <base href='<?php echo base_url(); ?>'>
-    <link rel="stylesheet" href="CSS/templates/header.css">
-    <link rel="stylesheet" href="CSS/templates/footer.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="JS/templates/header.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/templates/header.css">
+    <link rel="stylesheet" href="CSS/templates/footer.css">
+    <script src="JS/templates/header.js"></script>
 </head>
 
 <body>
